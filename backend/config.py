70c3e525096671acb9a76ffc8c2fea9af5e24d67
AkshayaaVeena <akshayaaveena225@gmail.com>
2025-10-14 10:19:29 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,8 @@
 from pathlib import Path
 import os
 
-# PCAP file output
 PCAP_OUTPUT = Path("captures/notifications.pcap")
 
-# Notification log file
 NOTIFICATION_LOG = Path("notifications.log")
 
-# Capture interface
 CAPTURE_INTERFACE = "Ethernet 4"  # Update to your interface if needed
